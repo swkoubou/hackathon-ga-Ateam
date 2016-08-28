852b156c9f22b4142f04fa8fe724ce4dffcb16c4
hisatugu <syou.0621s@gmail.com>
2016-08-28 12:20:18 +0900

    Add files via upload

---
+++ %E4%B9%85%E6%AC%A1/jump.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class jump : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnClick()
+    {
+
+       // SceneManager.LoadScene("main");
+        Debug.Log("i");
+    }
+}
